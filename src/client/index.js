ba00128f73f8eab626faf04e3746716b8277c570
Thiruphathy P <tpushparaj@walmart.com>
2020-02-06 09:46:04 -0800

    first react js

---
+++ src/client/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class App extends Component {
+    return <h1>Hello, World</h1>;
+}
+
+render(<App />, document.getElementById('root'));
